18b12a83383dfc83621a4317a4ec194c6a0f40c4
Money <48343592+45rfew@users.noreply.github.com>
2019-11-12 18:28:11 +1100

    Update and rename 504 - Cyborg.js to 503 - Cyborg.js

--- 503 - Cyborg.js
+++ 503 - Cyborg.js
@@ -7,15 +7,15 @@ t = -20
 model = 
   name: "Cyborg"
   level: 5
-  model: 5
+  model: 3
   size: 1.9
   specs:
     shield:
       capacity: [230,270]
       reload: [15,20]
     generator:
-      capacity: [300,465]
-      reload: [200,300]
+      capacity: [300,400]
+      reload: [50,100]
     ship:
       mass: 350
       speed: [130,150]
@@ -29,13 +29,13 @@ model =
         y:-25-h
         z:-2
       position:
-        x:[0,0,0,0,0,0,0,0,0,0,0]
-        y:[0,35,60,80,150,160,170,150,0,0,0]
-        z:[0,0,0,0,0,0,0,0,0,0,0]
-      width:[0,17,17,21,25,21,17,0]
-      height:[0,10,12,12,12,12,12,0]
+        x:[0,0,0,0,0,0,0,0,0,0,0,0]
+        y:[0,35,60,80,82,150,160,170,150,0,0,0]
+        z:[0,0,0,0,0,0,0,0,0,0,0,0]
+      width:[0,17,17,21,21,25,21,17,0]
+      height:[0,10,12,12,12,12,12,12,0]
       propeller: true
-      texture:[3,8,13,11,13,13,17]
+      texture:[3,8,13,17,11,13,13,17]
     cockpit:
       section_segments: 12
       offset:
@@ -44,7 +44,7 @@ model =
         z: -11
       position:
         x: [0,0,0,0,0,0,0,0]
-        y: [-30,-20,0,10,30,50]
+        y: [-20,-15,0,10,30,50]
         z: [-3,-2,0,0,0,0]
       width: [0,4,10,12,7,0]
       height: [0,5-3,7-3,9-3,5-2,0]
@@ -62,7 +62,7 @@ model =
       width: [0,0,0,10-s,13-s,14-s,15-s,17-s,19-s,17-s,15-s,13-s,0]
       height: [0,0,0,10-d,13-d,14-d,15-d,17-d,19-d,17-d,15-d,13-d,0]
       propeller: false
-      angle:2
+      angle: 0
       texture: [63,63,63,63,63,63,63,63,4,10,4]  
     thingy:
       section_segments: 6
@@ -109,18 +109,18 @@ model =
       propeller: false
       texture: [3,3,10,3]      
     turbine:
-      section_segments:10
+      section_segments: 10
       offset:
-        x:47-4
-        y:46-h
-        z:0
+        x: 47-4
+        y: 46-h
+        z: 0
       position:
-        x:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        y:[4*a,10*a,9*a,5*a,5*a,40*a,40*a,35*a,33*a,40*a,38*a]
-        z:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-      width:[0,1*a,5*a-q,5*a-q,6*a-q,6*a-q,5*a-q,5*a-q,3,2,0]
-      height:[0,1*a,5*a,5*a,6*a,6*a,5*a,5*a,3,2,0]
-      propeller:true
+        x: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        y: [4*a,10*a,9*a,5*a,5*a,40*a,40*a,35*a,33*a,40*a,38*a]
+        z: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+      width: [0,1*a,5*a-q,5*a-q,6*a-q,6*a-q,5*a-q,5*a-q,3,2,0]
+      height: [0,1*a,5*a,5*a,6*a,6*a,5*a,5*a,3,2,0]
+      propeller: true
       texture:[4,12,4,2,8,2,4,12,4,17]
   wings:
     main:
@@ -130,7 +130,7 @@ model =
       position: [0,10*3]
       offset:
         x: 0
-        y:80-h
+        y: 82-h
         z: 0
       texture: [11]
       doubleside: true
@@ -144,13 +144,54 @@ model =
       position: [0,20*3]
       offset:
         x: 63
-        y:70-h
+        y: 70-h
         z: 0
       texture: [63]
       doubleside: true
       bump:
         position: 60 
-        size: 10      
+        size: 10 
+    main3:
+      length: [70]
+      width: [35*2,12.5*2]
+      angle: [0,0]
+      position: [0,10*3]
+      offset:
+        x: 0
+        y: 80-h
+        z: 0
+      texture: [17]
+      doubleside: true
+      bump:
+        position: 30 
+        size: 0  
+    winglets:
+      length: [20]
+      width: [20,10,15]
+      angle: [-10,10]
+      position: [-40,-30,-55]
+      bump:
+        position: 10
+        size: 20
+      texture: 63
+      offset:
+        x: 10
+        y: 12
+        z: 1   
+for i in [0...10]
+  model.bodies['cannon_z'+i] =
+    section_segments: 20
+    offset:
+      x: 63
+      y: 10*i-10
+      z: 0
+    position:
+      x: [0,0,0,0,0]
+      y: [0,0,2,4,1]
+      z: [0,0,0,0,0]
+    width: [0,9,9,9,0]
+    height: [0,9,9,9,0]
+    texture: [63,63,17]
 if(1)
   for k,v of model.bodies
     v.offset.x=-v.offset.x
