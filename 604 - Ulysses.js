958a120a30df9ae208e8129c5a7c9990aeb7afa5
Money <48343592+45rfew@users.noreply.github.com>
2020-02-18 16:21:14 +1100

    Update 604 - Ulysses.js

--- 604 - Ulysses.js
+++ 604 - Ulysses.js
@@ -1,3 +1,50 @@
+r = 1.2
+t = 1
+s = 20
+ypos = []
+for i in [0...50] by 0.14
+  ypos.push(i*1)
+xpos = []
+for i in [0...5*s] by 0.4
+  xpos.push(r*Math.sin(i))
+zpos = []
+for i in [0...5*s] by 0.4
+  zpos.push(r*Math.cos(i))
+tub = []
+for i in [0...50*s] by 10
+  tub.push(t)
+spring = (px,py,pz,ang)->
+  angle: ang
+  section_segments: 6
+  offset:
+    x: px
+    y: py
+    z: pz
+  position:
+    x: xpos
+    y: ypos
+    z: zpos
+  width: tub
+  height: tub
+  texture: [[17]]
+  vertical: !true
+Array::add = (mod) -> @map (val) -> val + mod
+Array::mult = (mod) -> @map (val) -> val * mod
+Array::div = (mod) -> @map (val) -> val / mod
+Array::step = (step) -> i for i in @ by step
+Array::fill = (val, len) -> val for i in (
+  if len
+  then [0...len]
+  else if @length == 1
+  then [0...@[0]]
+  else @
+)
+for key, value of []
+  delete Array::[key]
+  Object.defineProperty Array::, key,
+    value: value
+    configurable: true
+    enumerable: false
 a = 2.5
 b = 5
 model =
@@ -191,11 +238,11 @@ model =
         z: 9
       position:
         x: [26,26,26,18,-13,-13]
-        y: [-30,-30,-10,10,60,60]
+        y: [-31,-31,-10,10,60,60]
         z: [0,0,2,12,-9,-9]
       width: [0,2,2,2,2,0]
       height: [0,2,2,2,2,0]
-      texture: [4]
+      texture: [[15]]
       angle: 90      
     detail11:
       section_segments: [45,135,225,315]
@@ -209,7 +256,7 @@ model =
         z: [0,0,2,12,-8,-8]
       width: [0,2,2,2,2,0]
       height: [0,2,2,2,2,0]
-      texture: [4]
+      texture: [[15]]
       angle: 90  
     detail12:
       section_segments: [45,135,225,315]
@@ -223,7 +270,7 @@ model =
         z: [0,2,-5,-7,9,9]
       width: [0,2,2,2,2,0]
       height: [0,2,2,2,2,0]
-      texture: [4]
+      texture: [[15]]
       angle: 90      
     detail13:
       section_segments: [45,135,225,315]
@@ -237,7 +284,7 @@ model =
         z: [0,2,-5,-7,8,8]
       width: [0,2,2,2,2,0]
       height: [0,2,2,2,2,0]
-      texture: [4]
+      texture: [[15]]
       angle: 90  
     detail14:
       section_segments: 12
@@ -251,7 +298,7 @@ model =
         z: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
       width: [0,13,13,11,11,13,11,11,13,11,11,13,13,11,0]
       height: [0,13,13,11,11,13,11,11,13,11,11,13,13,11,0]
-      texture: [4,4,4,7,4,4,7,4,4,7,4,4,17]
+      texture: [4,4,4,7,[15],[15],7,[15],[15],7,4,4,17]
       propeller: true
     detail15:
       section_segments: [15,65,115,165,195,245,295,345]
@@ -265,7 +312,7 @@ model =
         z: [0,0,0,0,0,0,0]
       width: [0,10,10,10,10,8,0]
       height: [0,18,18,18,18,16,0]
-      texture: [3.9,63,3,63,16.94]
+      texture: [[15],63,3,63,16.94]
       propeller: true      
     detail16:
       section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
@@ -303,7 +350,7 @@ model =
         angle: 1
         error: 2
     detail18:
-      section_segments: [30,35,40,130,135,140,220,225,230,320,325,330]
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
       offset:
         x: 19.5
         y: 115
@@ -354,7 +401,20 @@ model =
         z: [0,0,5,6,5,5]
       width: [0,2,2,2,0.75,0]
       height: [0,1,1,1,1,0]
-      texture: [63,63,4,3]  
+      texture: [63,63,4,3] 
+    detail21_5:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 19.5
+        y: 142
+        z: 23
+      position:
+        x: [0,0,0,0,0,0]
+        y: [-5,-5,-0,5,10,10]
+        z: [0,0,5,6,5,5]
+      width: [0,2,2,2,0.75,0].mult(1.4)
+      height: [0,1,1,1,1,0].div(2)
+      texture: [17]       
     detail22:
       section_segments: [45,135,225,315]
       offset:
@@ -368,6 +428,19 @@ model =
       width: [0,2,2,2,0.75,0]
       height: [0,1,1,1,1,0]
       texture: [63,63,4,3] 
+    detail22_5:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 25
+        y: 142
+        z: 15.2
+      position:
+        x: [0,0,5,6,5,5]
+        y: [-5,-5,-0,5,10,10]
+        z: [0,0,0,0,0,0]
+      width: [0,2,2,2,0.75,0].mult(1.4)
+      height: [0,1,1,1,1,0].div(2)
+      texture: [17]       
     detail23:
       section_segments: [45,135,225,315]
       offset:
@@ -380,7 +453,20 @@ model =
         z: [0,0,0,0,0,0]
       width: [0,2,2,2,0.75,0]
       height: [0,1,1,1,1,0]
-      texture: [63,63,4,3]  
+      texture: [63,63,4,3] 
+    detail23_5:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 14
+        y: 142
+        z: 15.2
+      position:
+        x: [0,0,-5,-6,-5,-5]
+        y: [-5,-5,-0,5,10,10]
+        z: [0,0,0,0,0,0]
+      width: [0,2,2,2,0.75,0].mult(1.4)
+      height: [0,1,1,1,1,0].div(2)
+      texture: [17]       
     detail24:
       section_segments: [45,135,225,315]
       offset:
@@ -421,7 +507,21 @@ model =
       width: [0,25,14,0]
       height: [0,1,1,0]
       texture: [3]     
-      angle: 90      
+      angle: 90 
+    detail26_5:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 57
+        y: 115
+        z: 12.99
+      position:
+        x: [0,4,0,0]
+        y: [-10,-10,9,9]
+        z: [8,8,0,1]
+      width: [0,26.7,15,0]
+      height: [0,1,1,0]
+      texture: [17]     
+      angle: 90       
     detail27:
       section_segments: [45,135,225,315]
       offset:
@@ -435,7 +535,35 @@ model =
       width: [0,34,25,0]
       height: [0,1,1,0]
       texture: [1]
-      angle: 90        
+      angle: 90  
+    detail27_5:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 40
+        y: 111
+        z: 13.1
+      position:
+        x: [5.5,8,0,0]
+        y: [-13.1,-10,7,7]
+        z: [-1.4,-0.45,8,8]
+      width: [36,36,27,0]
+      height: [0,1.03,1,0]
+      texture: [17]
+      angle: 90   
+    detail27_55:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 40
+        y: 111
+        z: 21.2
+      position:
+        x: [8,8,0,0]
+        y: [-10,-10,7,7]
+        z: [-10,-10,0,0]
+      width: [0,36,27,0]
+      height: [0,1.03,1,0]
+      texture: [17]
+      angle: 90         
     detail28:
       section_segments: [45,135,225,315]
       offset:
@@ -450,8 +578,399 @@ model =
       height: [0,2,10,10,0]
       texture: [1,1,63]
       angle: 1
-           
-      
+    detail29:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 19.5
+        y: 14
+        z: -120
+      position:
+        x: [0,0,0,0,0]
+        y: [0,0,40,37,37].div(3)
+        z: [0,0,0,0,0]
+      width: [0,30,10,2,0].div(3)
+      height: [0,100,80,80/3,0].div(5)
+      angle: 0
+      vertical: true
+      texture: [17,4,16.93]
+    disc:
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
+      offset:
+        x: 19.5
+        y: 124+2
+        z: 18
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].mult(.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].mult(1.05)
+      height: [10,10,10,12,12,12,12,12,10,10].mult(.8)
+      texture: [4,4,4,4,4,17,4,4]     
+    disc2:
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
+      offset:
+        x: 19.5
+        y: 112+3
+        z: 18
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].mult(.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].mult(1.05)
+      height: [10,10,10,12,12,12,12,12,10,10].mult(.8)
+      texture: [4,4,4,4,4,17,4,4]          
+    disc3:
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
+      offset:
+        x: 19.5
+        y: 118+2.5
+        z: 18
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].mult(.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].mult(1.05)
+      height: [10,10,10,12,12,12,12,12,10,10].mult(.8)
+      texture: [4,4,4,4,4,17,4,4]
+    disc3_5:
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
+      offset:
+        x: 19.5
+        y: 110-.5
+        z: 18
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].mult(.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].mult(1.05)
+      height: [10,10,10,12,12,12,12,12,10,10].mult(.8)
+      texture: [4,4,4,4,4,17,4,4]       
+    disc4:
+      section_segments: [20,40,60,120,140,160,180,200,220,240,260,280,300,320,340,360]
+      offset:
+        x: 9
+        y: 4.5
+        z: -35
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(3)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(3)
+      height: [10,10,10,12,12,12,12,12,10,10].div(3)
+      texture: [4,16.8,4,4,4,16.8,3.8,4]
+      vertical: true
+      angle: 60
+    disc5:
+      section_segments: [20,40,60,120,140,160,180,200,220,240,260,280,300,320,340,360]
+      offset:
+        x: 9
+        y: 4.5
+        z: -45
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(3)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(3)
+      height: [10,10,10,12,12,12,12,12,10,10].div(3)
+      texture: [4,16.8,4,4,4,16.8,3.8,4]
+      vertical: true
+      angle: 60  
+    disc6:
+      section_segments: 6
+      offset:
+        x: 1
+        y: 9
+        z: -91.6
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(3)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(3)
+      height: [10,10,10,12,12,12,12,12,10,10].div(1.8)
+      texture: [4,16.8,4,4,4,16.8,3.8,4]
+      vertical: true
+      angle: 90   
+    disc7:
+      section_segments: 10
+      offset:
+        x: -1
+        y: 86
+        z: 10
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(3)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(3)
+      height: [10,10,10,12,12,12,12,12,10,10].div(3)
+      texture: [4,16.8,4,4,4,16.8,3.8,4]
+      vertical: !true
+      angle: 90+45  
+    disc8:
+      section_segments: 6
+      offset:
+        x: 0.9
+        y: 9
+        z: -79
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(3)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(3)
+      height: [10,10,10,12,12,12,12,12,10,10].div(2)
+      texture: [4,16.8,4,4,4,16.8,3.8,4]
+      vertical: true
+      angle: 90   
+    disc9:
+      section_segments: 6
+      offset:
+        x: 0.01
+        y: 98.3
+        z: 10
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(3)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(3)
+      height: [10,10,10,12,12,12,12,12,10,10].div(3)
+      texture: [4,16.8,4,4,4,16.8,3.8,4]
+      vertical: !true
+      angle: 90-20 
+    disc10:
+      section_segments: 6
+      offset:
+        x: 0
+        y: 9
+        z: -115
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(3).add(-1.3).mult(1.1)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(3)
+      height: [10,10,10,12,12,12,12,12,10,10].div(1.2).mult(2.1)
+      texture: [4,16.8,4,4,4,16.8,3.8,4]
+      vertical: true
+      angle: 90 
+    disc11:
+      section_segments: 20
+      offset:
+        x: 99
+        y: 106.5+1
+        z: 8
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(2.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(1.6)
+      height: [10,10,10,12,12,12,12,12,10,10].div(1.1)
+      texture: [4,4,4,4,4,17,4,4]
+    disc12:
+      section_segments: 20
+      offset:
+        x: 99
+        y: 110.5+1
+        z: 8
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(2.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(1.6)
+      height: [10,10,10,12,12,12,12,12,10,10].div(1.1)
+      texture: [4,4,4,4,4,17,4,4]
+    disc13:
+      section_segments: [15,65,115,165,195,245,295,345]
+      offset:
+        x: 100
+        y: 122
+        z: 0
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(2.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(1.1)
+      height: [10,10,10,12,12,12,12,12,10,10].div(0.65)
+      texture: [17,17.87,17,17,17,17.87,17,17]     
+    disc14:
+      section_segments: [15,65,115,165,195,245,295,345]
+      offset:
+        x: 100
+        y: 100
+        z: 0
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [7.5,7.5,1,1,1,2,6,7.5,7.5,7.5].div(2.5)
+        z: [0,0,0,0,0,0,0,0,0,0]
+      width: [10,10,10,12,12,12,12,12,10,10].div(1.1)
+      height: [10,10,10,12,12,12,12,12,10,10].div(0.65)
+      texture: [17,17.87,17,17,17,17.87,17,17]       
+    rod:
+      section_segments: [45,135,225,315].add(20)
+      offset:
+        x: 10.5
+        y: 40
+        z: 5.5
+      position:
+        x: [0,0,0,0,0.2,0.5,0,0,0,0.5,0.2,0,0,0,0]
+        y: [-10,-10,-8.5,-7,-5,-4,-2,0,2,4,5,7,8.5,10,10]
+        z: [0,0,0,0,-0.5,-1,0,0,0,-1,-0.5,0,0,0,0]
+      width: [0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0]
+      height: [0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0]
+      texture: [4]            
+    circ:
+      section_segments: 30
+      offset:
+        x: 4.5
+        y: 2
+        z: -30
+      position:
+        x: [0,0,0,0,0,0,0]
+        y: [0,0,40,40,40,40,40].div(5)
+        z: [0,0,0,0,0,0,0]
+      width: [0,12,12,9,2,0].div(5)
+      height: [0,12,12,9,2,0].div(5)
+      angle: 60
+      vertical: true
+      texture: [17,4,16.93,17.93]  
+    circ2:
+      section_segments: 30
+      offset:
+        x: 4.5
+        y: 2
+        z: -40
+      position:
+        x: [0,0,0,0,0,0,0]
+        y: [0,0,40,40,40,40,40].div(5)
+        z: [0,0,0,0,0,0,0]
+      width: [0,12,12,9,2,0].div(5)
+      height: [0,12,12,9,2,0].div(5)
+      angle: 60
+      vertical: true
+      texture: [17,4,16.93,17.93]  
+    circ3:
+      section_segments: 30
+      offset:
+        x: 4.5
+        y: 2
+        z: -50
+      position:
+        x: [0,0,0,0,0,0,0]
+        y: [0,0,40,40,40,40,40].div(5)
+        z: [0,0,0,0,0,0,0]
+      width: [0,12,12,9,2,0].div(5)
+      height: [0,12,12,9,2,0].div(5)
+      angle: 60
+      vertical: true
+      texture: [17,4,16.93,17.93] 
+    hub:
+      section_segments: 20
+      offset:
+        x: 0
+        y: 10
+        z: -120
+      position:
+        x: [0,0,0,0,0,0,0]
+        y: [0,10,5,5,10,6].div(2)
+        z: [0,0,0,0,0,0,0]
+      width: [18,15,13,12,10,0].div(2)
+      height: [18,15,13,12,10,0].div(2)
+      texture: [18,17,17,18,18]  
+      vertical: true
+    hub2:
+      section_segments: 20
+      offset:
+        x: 105+1
+        y: 10
+        z: -113+2
+      position:
+        x: [0,0,0,0,0,0,0]
+        y: [0,10,5,5,10,6].div(3)
+        z: [0,0,0,0,0,0,0]
+      width: [18,15,13,12,10,0].div(3)
+      height: [18,15,13,12,10,0].div(3)
+      texture: [18,17,17,18,18]  
+      vertical: true      
+      angle: 55
+    bar:
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
+      offset:
+        x: 4
+        y: 70
+        z: 10
+      position:
+        x: [4,4,4,4,2,2,0,0,0]
+        y: [-4,-4,0,18,22,30,35,60,60]
+        z: [0,0,0,0,0,0,0,0,0]
+      width: [0,2,2.5,2.5,2.5,2.5,2.5,2.5,0]
+      height: [0,0.5,2.5,2.5,2.5,2.5,2.5,2.5,0]
+      texture: [18.17,17,18.17,4,18.17,17,4]
+    bar2:
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
+      offset:
+        x: 0.001
+        y: 70
+        z: 10
+      position:
+        x: [4,4,3,3,2,2,0,0,0]
+        y: [-4,-4,0,18,22,30,35,50,50]
+        z: [0,0,0,0,0,0,0,0,0]
+      width: [0,2,3.5,3.5,2.5,2.5,2.5,2.5,0].mult(1.299)
+      height: [0,0.5,2.5,2.5,2.5,2.5,2.5,2.5,0].div(2)
+      texture: [[15]]
+    bar3:
+      section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
+      offset:
+        x: 107
+        y: 97.5
+        z: 0
+      position:
+        x: [0,0,0,0,1.5,0]
+        y: [0,0,10,30,40,40].div(1.3)
+        z: [0,0,0,0,0,0]
+      width: [0,3,6,6,1,0]
+      height: [0,6,6,6,6,0]
+      texture: [8,8,4,8]   
+    bar4:
+      section_segments: [35,45,55,125,135,145,215,225,235,305,315,325]
+      offset:
+        x: 109
+        y: 105.5
+        z: 0
+      position:
+        x: [0,0,0,0,0,0]
+        y: [0,0,10,30,40,40].div(2.8)
+        z: [0,0,0,0,0,0]
+      width: [0,3,6,6,3,0]
+      height: [0,7,7,7,7,0]
+      texture: [3,3,17.95,3]         
+    engine_detail:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 17
+        y: 82
+        z: 10
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0]
+        y: [0,0,8,10,12,15,17,20,22,22]
+        z: [0,0,10,10,10,10,10,10,10]
+      width: [0,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,0]
+      height: [0,2,2,2,2,2,2,2,2,0]
+      texture: [5,18.2,13,18,13,18,13,18]          
+    engine_detail2:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 22
+        y: 82
+        z: 10
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0]
+        y: [0,0,8,10,12,15,17,20,22,22]
+        z: [0,0,10,10,10,10,10,10,10]
+      width: [0,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,0]
+      height: [0,2,2,2,2,2,2,2,2,0]
+      texture: [5,18.2,13,18,13,18,13,18]         
+    spring: spring(22,90,20,0)
+    spring2: spring(17,90,20,0) 
+    spring3: spring(22,90.3,20,0)
+    spring4: spring(17,90.3,20,0)        
   wings: 
     main:
       doubleside: true
@@ -480,7 +999,35 @@ model =
       texture: [63]
       bump:
         position: 35
-        size: 20            
+        size: 20   
+    main3:
+      doubleside: true
+      offset:
+        x: 100
+        y: 125
+        z: 7.01
+      length: [0,15/b]
+      width: [0/b,210/b,0/b]
+      angle: [90,90]
+      position: [-0,-30,-10]
+      texture: [17]
+      bump:
+        position: 35
+        size: 20    
+    main4:
+      doubleside: true
+      offset:
+        x: 100
+        y: 125
+        z: -7.1
+      length: [0,1/b]
+      width: [0/b,220/b,0/b]
+      angle: [90,90]
+      position: [-0,-30,-10]
+      texture: [17]
+      bump:
+        position: 35
+        size: 20        
     sides:
       length: [35]
       width: [13,8]
@@ -537,7 +1084,23 @@ model =
         position: 0
         size: 0
       texture: [63]       
-t = -50        
+for i in [1...9]
+  model.bodies['engine_shield'+i] =
+    section_segments: [0,45,55,125,135,225,235,305,315]
+    offset:
+      x: 0
+      y: 0
+      z: -154
+    position:
+      x: [0,0,0,0,0,0]
+      y: [5,5,10,10,10,11].add(5)
+      z: [0,0,0,-6,-6,-6]
+    width: [0,2,2,4,2.7,0].add(-1)
+    height: [0,1,1,11,9.7,0]
+    texture: [17.8,17.8,[15],16.8,[15]]
+    angle: i*45
+    vertical: true    
+t = -50    
 if(1)
   for y,v of model.bodies
     if(v.vertical)
@@ -547,4 +1110,4 @@ if(1)
   for y,v of model.wings
     v.offset.y = v.offset.y + t      
 return model;        
-      
+    
