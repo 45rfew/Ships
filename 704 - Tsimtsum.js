723d9bc0b8267636479744af952f613e4dbdb8f8
Money <48343592+45rfew@users.noreply.github.com>
2020-04-29 14:56:37 +1000

    Update and rename 704 - Netflix.js to 704 - Tsimtsum.js

--- 704 - Tsimtsum.js
+++ 704 - Tsimtsum.js
@@ -1,5 +1,38 @@
-return model =
-  name: "Netflix"
+Array::add = (mod) -> @map (val) -> val + mod
+Array::mult = (mod) -> @map (val) -> val * mod
+Array::div = (mod) -> @map (val) -> val / mod
+Array::step = (step) -> i for i in @ by step
+Array::fill = (val, len) -> val for i in (
+  if len
+  then [0...len]
+  else if @length == 1
+  then [0...@[0]]
+  else @
+)
+for key, value of []
+  delete Array::[key]
+  Object.defineProperty Array::, key,
+    value: value
+    configurable: true
+    enumerable: false
+ring = (xx, yy, zz, segments, l, w, t, tcc, vert, tx, angle, z) ->
+  return k =
+    section_segments: segments
+    offset:
+      x: xx
+      y: yy
+      z: zz
+    position:
+      x: [].fill(0,13)
+      y: [-2.25,-1.75,-1.25,0,1.25,1.75,2.25,1.75,1.25,0,-1.25,-1.75,-2.25].mult(t+tcc)
+      z: [].fill(0,13)
+    width: [w,w+1*t,w+2*t,w+2.5*t,w+2*t,w+1*t,w,w-1*t,w-2*t,w-2.5*t,w-2*t,w-1*t,w]
+    height: [l,l+1*t,l+2*t,l+2.5*t,l+2*t,l+1*t,l,l-1*t,l-2*t,l-2.5*t,l-2*t,l-1*t,l]
+    vertical: vert
+    texture: tx
+    angle: angle
+model =
+  name: "Tsimtsum"
   level: 7
   model: 4
   size: 1.7
@@ -16,6 +49,9 @@ return model =
       rotation: [130,130]
       acceleration: [70,70]
   bodies:
+    ring: ring(0,-98,15,18,10,15,1.5,.2,0,16.9,0)
+    ring2: ring(19,-115,15,6,7,7,.5,.5,0,16.9,0)
+    ring3: ring(19,-105,15,6,7,7,.5,.5,0,16.9,0)
     main:
       section_segments: 8
       offset:
@@ -23,11 +59,11 @@ return model =
         y: 90
         z: -5
       position:
-        x: [0,0,0,0,0,0,0,0,0,0]
-        y: [-215,-210,-200,-170,10,30,55,75,60]
-        z: [0,0,0,0,0,0,0,0,0]
-      width: [0,8,45,35,40,35,30,25,0]
-      height: [0,6,20,20,20,20,20,20,0]
+        x: [0,0,0,0,0,0,0,0,0,0,0]
+        y: [-215,-210,-200,-170,10,30,55,75,75,60]
+        z: [0,0,0,0,0,0,0,0,0,0,0]
+      width: [0,8,45,35,40,35,30,25,24,0]
+      height: [0,6,20,20,20,20,20,20,19,0]
       propeller: true
       texture: [4,63,4,4,4,4,12,17]
     side_main:
@@ -42,7 +78,6 @@ return model =
         z: [0,0,0,0,0,0,0,0,0]
       width: [0,8,45,35,40,35,30,25,0]
       height: [0,6,20,20,20,20,20,20,0]
-      propeller: true
       texture: [4,4,4,18,4,4,4,17]
     head:
       section_segments: [0,55,90,150,210,270,305]
@@ -58,6 +93,32 @@ return model =
       height: [0,25,25,0]
       propeller: false
       texture: [63]
+    cockpit:
+      section_segments: 6
+      offset:
+        x: 0
+        y: -150
+        z: 12
+      position:
+        x: [0,0,0,0,0,0,0,0]
+        y: [-27.85,-17,17.85,37.71,50,50]
+        z: [-8.25,-7.46,0,0,0,0]
+      width: [0,8.92,13.39,13.39,13,0]
+      height: [0,8.92,13.39,15.71,14,0]
+      texture: [7,9,9,4]
+    shield:
+      section_segments: [0,60,120,180]
+      offset:
+        x: -9
+        y: -105
+        z: 0
+      position:
+        x: [5,5,-2,-2,-2,-2]
+        y: [-73,-73,-30,-15,5,5]
+        z: [0,-1,1,5,5,5]
+      width: [0,5,15,15,15,0]
+      height: [0,11,20,20,20,0]
+      texture: [16.9,3.9,18,2.9]       
     bars:
       section_segments: 8
       offset:
@@ -71,7 +132,7 @@ return model =
       width: [0,2,7.5,7.5,7.5,8.75,8.75,8.75,8.75,5,5,0]
       height: [0,1.5,5,5,5,5,5,5,5,5,5,0]
       propeller: false
-      texture: [4,63,1,1,2,3,2,2,13,4]
+      texture: [4,63,18,4,18,8,18,17,13,4]
     under_bars:
       section_segments: 8
       offset:
@@ -85,33 +146,20 @@ return model =
       width: [0,2,7.5,7.5,7.5,8.75,8.75,5,5,0]
       height: [0,1.5,5,5,5,5,5,5,5,0]
       propeller: false
-      texture: [4,63,1,1,2,2,13,4]
-    cockpit:
-      section_segments: 8
-      offset:
-        x: 0
-        y: -150
-        z: 12
-      position:
-        x: [0,0,0,0,0,0,0]
-        y: [-17.85,0,17.85,35.71,44.64]
-        z: [-6.25,-4.46,0,0,0]
-      width: [0,8.92,13.39,13.39,0]
-      height: [0,8.92,13.39,11.71,0]
-      texture: [7,9,9,7]
-    cannons1:
+      texture: [4,63,18,4,4,17,13,4]
+    cannons:
       section_segments: 12
       offset:
         x: 160
         y: 99
         z: 36
       position:
-        x: [0,0,0,0,0,0,0,0,0,0,0]
-        y: [-105,-65,-70,-57,-55,-3,0,5,10,30,20]
-        z: [0,0,0,0,0,0,0,0,0,0,0]
-      width: [0,12,15,15,13,15,16,16,12,12,0]
-      height: [0,12,15,15,13,15,16,16,12,12,0]
-      texture: [3,17,2,13,1,63,63,63,13,17]
+        x: [0,0,0,0,0,0,0,0,0,0,0,0]
+        y: [-105,-65,-70,-57,-55,-3,0,5,10,30,30,20]
+        z: [0,0,0,0,0,0,0,0,0,0,0,0]
+      width: [0,12,15,15,13,15,16,16,12,12,11,0]
+      height: [0,12,15,15,13,15,16,16,12,12,11,0]
+      texture: [3,17,63,63,4,63,63,63,13,17]
       propeller: true
       angle: 4
       laser:
@@ -128,12 +176,12 @@ return model =
         y: 84
         z: -15
       position:
-        x: [0,0,0,0,0,0,0,0,0,0,0]
-        y: [-100,-65,-70,-57,-55,-3,0,5,10,25,15]
-        z: [0,0,0,0,0,0,0,0,0,0,0]
-      width: [0,10,12.5,12.5,10.83,12.5,13.3,13.3,10,10,0]
-      height: [0,10,12.5,12.5,10.83,12.5,13.3,13.3,10,10,0]
-      texture: [3,17,2,13,1,2,2,2,13,17]
+        x: [0,0,0,0,0,0,0,0,0,0,0,0]
+        y: [-100,-65,-70,-57,-55,-3,0,5,10,25,25,15]
+        z: [0,0,0,0,0,0,0,0,0,0,0,0]
+      width: [0,10,12.5,12.5,10.83,12.5,13.3,13.3,10,10,9,0]
+      height: [0,10,12.5,12.5,10.83,12.5,13.3,13.3,10,10,9,0]
+      texture: [3,17,63,63,4,63,63,63,13,17]
       propeller: true
       angle: 3
       laser:
@@ -150,26 +198,12 @@ return model =
         y: 110
         z: -5
       position:
-        x: [0,0,0,-2,-2,0]
-        y: [-50,10,20,36,50,40]
-        z: [10,0,0,0,0,0]
-      width: [0,6,7.5,7.5,6.5,0]
-      height: [0,12,15,15,13,0]
-      texture: [3,17,2,13,17,63,63,63,13,17]
-      propeller: true
-    side_propeller_flame:
-      section_segments: 8
-      offset:
-        x: 20
-        y: 160
-        z: -5
-      position:
-        x: [0]
-        y: [0]
-        z: [0]
-      width: [15]
-      height: [15]
-      texture: [0]
+        x: [0,0,0,-2,-2,-2,-2]
+        y: [-50,10,20,36,50,50,40]
+        z: [10,0,0,0,0,0,0]
+      width: [0,6,7.5,7.5,6.5,5,0]
+      height: [0,12,15,15,13,11.5,0]
+      texture: [3,17,3,13,17,17]
       propeller: true
     wing_holders:
       section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
@@ -185,25 +219,11 @@ return model =
       height: [0,25,25,25,0]
       propeller: false
       vertical: true
-      texture: [2]
-    main_support:
+      texture: [3]
+    blocc:
       section_segments: [45,135,225,315]
       offset:
-        x: 0
-        y: -90
-        z: 15
-      position:
-        x: [0,0,0,0,0,0]
-        y: [-25,-25,-20,0,25,25]
-        z: [0,0,0,0,0,0]
-      width: [0,12,12,12,12,12,0]
-      height: [0,3,3,3,3,0]
-      texture: [3,3,17,3,3]
-      angle: 90
-    main_support2:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 0
+        x: 0.01
         y: -89.99
         z: 15.01
       position:
@@ -226,7 +246,7 @@ return model =
         z: [-5,-5,0,0,0,0,-5]
       width: [0,12,12,12,12,12,0,0]
       height: [0,5,5,5,5,0,0]
-      texture: [63,63,2,63,3] 
+      texture: [63,63,3,63,3] 
     box:
       section_segments: [45,135,225,315]
       offset:
@@ -239,147 +259,7 @@ return model =
         z: [0,0,0,0,0,0]
       width: [0,13,13,13,13,0]
       height: [0,5,5,5,5,0]
-      texture: [1,9.96,2,3]       
-    box_deco:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -75
-        z: 18.8
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [63]    
-      angle: 90
-    box_deco2:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -74
-        z: 18.7
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [5]    
-      angle: 90      
-    box_deco3:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -65
-        z: 18.8
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [63]    
-      angle: 90
-    box_deco4:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -64
-        z: 18.7
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [5]    
-      angle: 90        
-    box_deco4:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -55
-        z: 18.8
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [63]    
-      angle: 90
-    box_deco5:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -54
-        z: 18.7
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [5]    
-      angle: 90        
-    box_deco6:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -45
-        z: 18.8
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [63]    
-      angle: 90  
-    box_deco7:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -44
-        z: 18.7
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [5]    
-      angle: 90        
-    box_deco8:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -35
-        z: 18.8
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [63]    
-      angle: 90  
-    box_deco9:
-      section_segments: [45,135,225,315]
-      offset:
-        x: 19
-        y: -34
-        z: 18.7
-      position:
-        x: [0,0,0,0,0]
-        y: [-10,-10,-7,10,10]
-        z: [0,0,-0.5,-10,-10]
-      width: [0,2.5,2.5,2.5,0]
-      height: [0,1,1,1,0]
-      texture: [5]    
-      angle: 90        
+      texture: [1,18.2,17,3]       
     back_support:  
       section_segments: [45,135,225,315]
       offset:
@@ -435,7 +315,7 @@ return model =
       width: [0,8,8,8,8,0]
       height: [0,1,1,1,1,0]
       texture: [17]
-    hubs:  
+    reactor:  
       section_segments: 30
       offset:
         x: 0
@@ -449,7 +329,7 @@ return model =
       height: [13,13,10,9,6,4.2,4,0]
       texture: [1,3,18,17,18,16,17]
       vertical: true
-    hub_support:  
+    reactor_joint:  
       section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
       offset:
         x: 0
@@ -462,7 +342,7 @@ return model =
       width: [0,1.5,1.5,1.5,1.5,0]
       height: [0,3,3,3,3,0]
       texture: [4]      
-    hub_support2:  
+    reactor_joint2:  
       section_segments: [40,45,50,130,135,140,220,225,230,310,315,320]
       offset:
         x: 0
@@ -476,6 +356,20 @@ return model =
       height: [0,3,3,3,3,0]
       texture: [4]       
       angle: 90
+    reactor2:  
+      section_segments: 30
+      offset:
+        x: 0
+        y: 50
+        z: -150
+      position:
+        x: [0,0,0,0,0,0,0,0]
+        y: [-20,-13,-10,-12,-12.5,-10,-10,-12].div(1.4)
+        z: [0,0,0,0,0,0,0,0]
+      width: [13,13,10,9,6,4.2,4,0]
+      height: [13,13,10,9,6,4.2,4,0]
+      texture: [1,3,18,17,18,16,17]
+      vertical: true      
     back:  
       section_segments: [40,45,50,130,135,140,220,225,310,315,320]
       offset:
@@ -545,19 +439,6 @@ return model =
       height: [8/1.5,8/1.5,7/1.5,6/1.5,4/1.5,1.2/1.5,1/1.5,0]
       texture: [1,3,18,17,18,16,17]
       vertical: true  
-    hub_supports:  
-      section_segments: [45,135,225,315]
-      offset:
-        x: 10.5
-        y: 71
-        z: 18
-      position:
-        x: [3,3,0,0,3,3]
-        y: [-7,-7,-5,5,7,7]
-        z: [0,0,0,0,0,0]
-      width: [0,1.5,1.5,1.5,1.5,0]
-      height: [0,3,3,3,3,0]
-      texture: [1,17,16,17,1]          
     cannon_lights:  
       section_segments: [45,135,225,315]
       offset:
@@ -823,51 +704,124 @@ return model =
       width: [0,0.5,0.5,0.5,0]
       height: [0,1,1,1,0]
       texture: [5]      
-      angle: 90        
+      angle: 90     
+    wing_bump:
+      section_segments: 6
+      offset:
+        x: 75
+        y: 186
+        z: 15
+      position:
+        x: [0,0,0,0,0]
+        y: [-3,4,4,-3,-3]
+        z: [0,0,0,0,0]
+      width: [13,13,11,11,13]
+      height: [5,5,0,0,5]
+      angle: 90
+      texture: [17,4]
+    wing_bump2:
+      section_segments: 6
+      offset:
+        x: 85
+        y: 188
+        z: 17
+      position:
+        x: [0,0,0,0,0]
+        y: [-3,4,4,-3,-3]
+        z: [0,0,0,0,0]
+      width: [11,11,9,9,11]
+      height: [5,5,0,0,5]
+      angle: 90
+      texture: [17,4]  
+    lights:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 15
+        y: 3
+        z: 38
+      position:
+        x: [0,0,0,0,0]
+        y: [-5,-5,15,16,16]
+        z: [0,0,0,0,0]
+      width: [0,3,3,2,0]
+      height: [0,53,53,51,0]
+      texture: [63,17,63]
+      angle: 30
+      vertical: true  
+    lights2:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 3
+        y: 16
+        z: 59
+      position:
+        x: [0,0,0,0,0]
+        y: [-5,-5,5,6,6]
+        z: [0,0,0,0,0]
+      width: [0,3,3,2,0]
+      height: [0,23,23,21,0]
+      texture: [63,17,63]
+      angle: 30
+      vertical: true    
+    lights3:
+      section_segments: [45,135,225,315]
+      offset:
+        x: 4
+        y: 13
+        z: -77
+      position:
+        x: [0,0,0,0,0]
+        y: [-5,-5,5,6,6]
+        z: [0,0,0,0,0]
+      width: [0,3,3,2,0]
+      height: [0,33,33,31,0]
+      texture: [63,17,63]
+      angle: 30
+      vertical: true          
   wings:
-    xwing1:
+    main:
       doubleside: true
       offset:
         x: 0
         y: 70
         z: 0
-      length: [45,20,2,12,23-3,55]
-      width: [70,60+1,61,61,58,50+5,45]
-      angle: [15,15,15,15,15,15]
-      position: [0,0,0,0,0,0,0]
-      texture: [8,3,4,2,1,10]
+      length: [45,2,20,2,12,2,20,2,55]
+      width: [70,61,61,61,61,58,58,55,55,45]
+      angle: [15,15,15,15,15,15,15,15,15]
+      position: [0,0,0,0,0,0,0,0,0,0]
+      texture: [8,17,4,17,18,17,8,17,18]
       bump:
-        position: 30
+        position: 20
         size: 10
-    xwing12:
+    main_lights:
       doubleside: true
       offset:
         x: 0
-        y: 75-8+2.5
-        z: -2.3
-      length: [100,55]
-      width: [70,60,50]
-      angle: [15,15]
-      position: [0,0,0]
-      texture: [17,17]
-      bump:
-        position: 30
-        size: 9 
-    xwing13:
-      doubleside: true
-      offset:
-        x: 0
-        y: 75-2.5
+        y: 68
         z: 0
-      length: [100,55]
-      width: [70,60,50]
-      angle: [15,15]
-      position: [0,0,0]
-      texture: [18]
+      length: [45,2,20,2,12,2,20,2,55]
+      width: [70,61,61,61,61,58,58,55,55,45]
+      angle: [15,15,15,15,15,15,15,15,15]
+      position: [0,0,0,0,0,0,0,0,0,0]
+      texture: [17]
       bump:
         position: 30
-        size: 0       
-    xwing2:
+        size: 0
+    main2:
+      doubleside: true
+      offset:
+        x: 0
+        y: 71
+        z: 0
+      length: [45,2,20,2,12,2,20,2,54]
+      width: [70,61,61,61,61,58,58,55,55,45]
+      angle: [15,15,15,15,15,15,15,15,15]
+      position: [0,0,0,0,0,0,0,0,0,0]
+      texture: [8,17,4,17,4,17,3,17,4]
+      bump:
+        position: 20
+        size: 10    
+    bottom:
       doubleside: true
       offset:
         x: 0
@@ -877,49 +831,82 @@ return model =
       width: [70-2.5,60-2.5,50-2.5]
       angle: [-15,-10]
       position: [0,-10,-17.5]
-      texture: [1,10]
+      texture: [4,17.8]
       bump:
         position: 30
         size: 10
-    xwing22:
+    bottom_lights:
       doubleside: true
       offset:
         x: 0
-        y: 75-8+1.75
-        z: 4
+        y: 68
+        z: 5
       length: [35,50]
-      width: [70,60,50]
+      width: [70-2.5,60-2.5,50-2.5]
       angle: [-15,-10]
       position: [0,-10,-17.5]
-      texture: [1,17]
+      texture: [17]
       bump:
         position: 30
-        size: 9        
-    xwing3:
+        size: 0      
+    top:
       doubleside: true
       offset:
         x: 0
         y: 170
         z: 30
-      length: [25,50,35,100]
-      width: [70,60,50,20]
-      angle: [15,-30,20]
-      position: [-15,-13,10,20]
-      texture: [3,10,1]
+      length: [25,2,50,2,35,100]
+      width: [70,60,60,50,48,20]
+      angle: [15,-30,-30,20,20]
+      position: [-15,-13,-13,10,10,20]
+      texture: [4,17,18,17,4]
       bump:
         position: 35
         size: 10
-    xwing32:
+    top_lights:
       doubleside: true
       offset:
         x: 0
-        y: 173
+        y: 168
         z: 30
-      length: [25,50,35,100]
-      width: [70,60,50,20]
-      angle: [15,-30,20]
-      position: [-15,-13,10,20]
-      texture: [4,18,18]
+      length: [25,2,50,2,35,100]
+      width: [70,60,60,50,48,20]
+      angle: [15,-30,-30,20,20]
+      position: [-15,-13,-13,10,10,20]
+      texture: [17]
       bump:
         position: 35
-        size: 9
+        size: 0      
+for i in [0..4]
+  model.bodies["asa"+i] = 
+    section_segments: [45,135,225,315]
+    offset:
+      x: 19
+      y: i*10+-75
+      z: 18.8
+    position:
+      x: [0,0,0,0,0]
+      y: [-10,-10,-7,10,10]
+      z: [0,0,-0.5,-10,-10]
+    width: [0,2.5,2.5,2.5,0]
+    height: [0,1,1,1,0]
+    texture: [63]    
+    angle: 90
+  model.bodies["as"+i] = 
+    section_segments: [45,135,225,315]
+    offset:
+      x: 19
+      y: i*10+-73.2
+      z: 18.7
+    position:
+      x: [0,0,0,0,0]
+      y: [-10,-10,-7,10,10]
+      z: [0,0,-0.5,-10,-10]
+    width: [0,2.5,2.5,2.5,0]
+    height: [0,1,1,1,0]
+    texture: [[15]]    
+    angle: 90   
+return model;    
+    
+    
+    
