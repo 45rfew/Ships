189472a8215baf81ab08d409a4dce4489a6ad09e
Money <48343592+45rfew@users.noreply.github.com>
2020-07-07 23:33:18 +1000

    Create Attack Pod.js

---
+++ Attack Pod.js
@@ -0,0 +1,81 @@
+return model =
+  name: 'Attack Pod'
+  level:1
+  model:1
+  size:1.05
+  specs:
+    shield:
+      capacity:[75,100]
+      reload:[2,3]
+    generator:
+      capacity:[40,60]
+      reload:[10,15]
+    ship:
+      mass:60
+      speed:[125,145]
+      rotation:[110,130]
+      acceleration:[100,120]
+  bodies:
+    main:
+      section_segments:[35,55,125,145,215,235,305,325,395]
+      offset:
+        x:0
+        y:0
+        z:0
+      position:
+        x:[0,0,0,0,0,0,0]
+        y:[-50,-50,0,25,50,40]
+        z:[0,0,0,0,0,0,0]
+      width:[0,10,50,50,20,0]
+      height:[0,5,20,15,15,0]
+      propeller:!1
+      texture:[1,1,4,3,17]
+    cannon:
+      section_segments:8
+      offset:
+        x:0
+        y:0
+        z:0
+      position:
+        x:[0,0,0,0,0,0,0]
+        y:[-60,-70,-30,0,30,35,40]
+        z:[0,0,0,5,5,0,0,0,0]
+      width:[0,10,15,10,15,10,0]
+      height:[0,10,15,18,15,10,0]
+      propeller:!1
+      texture:[4,63,1,1,1,4]
+   wings: I000l:    
+      length:[60]
+      width:[80,30]
+      angle:[0,0]
+      position:[0,20]
+      doubleside:!0      
+      texture:[63]
+      offset:
+        x:0
+        y:0
+        z:-5
+      bump:
+        position:30
+        size:20
+ 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    
