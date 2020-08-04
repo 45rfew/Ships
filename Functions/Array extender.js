35713b4ee3d5efcd8a540eae3326f0331f223f03
Money <48343592+45rfew@users.noreply.github.com>
2020-08-04 17:34:17 +1000

    Credits to Notus & Malefor

---
+++ Functions/Array extender.js
@@ -0,0 +1,17 @@
+Array::add = (mod) -> @map (val) -> val + mod
+Array::mult = (mod) -> @map (val) -> val * mod
+Array::div = (mod) -> @map (val) -> val / mod
+Array::step = (step) -> i for i in @ by step
+Array::fill = (val, len) -> val for i in (
+  if len
+  then [0...len]
+  else if @length == 1
+  then [0...@[0]]
+  else @
+)
+for key, value of []
+  delete Array::[key]
+  Object.defineProperty Array::, key,
+    value: value
+    configurable: true
+    enumerable: false
