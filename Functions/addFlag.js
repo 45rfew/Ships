04053a252c4e596f919e60144cc74024188967c7
45rfew <48343592+45rfew@users.noreply.github.com>
2020-09-24 20:09:18 +1000

    Create addFlag.js

---
+++ Functions/addFlag.js
@@ -0,0 +1,28 @@
+addFlag = (x=0,y=0,z=0,size=1,fz=0) ->
+  model.bodies["flag"] =  
+    section_segments: [44,45,46,135,225,310,315,320]
+    offset:
+      x: 0
+      y: 65+y
+      z: 79+z-fz
+    position:
+      x: [0,0,2,2,2,-2,-2,-2,2,2,2,-2,-2,-2,0].add(x).mult(size)
+      y: [0,0,18,20,22,38,40,42,58,60,62,78,80,82,82].mult(size)
+      z: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    width: [0,3,3,3,3,3,3,3,3,3,3,3,3,3,0].mult(size)
+    height: [0,40,40,40,40,40,40,40,40,40,40,40,40,40,0].mult(size)
+    texture: [63]   
+  model.bodies["flagpole"] = 
+    section_segments: 10
+    offset:
+      x: 0
+      y: 10+z
+      z: -65-y
+    position:
+      x: [0,0,0,0].add(x).mult(size)
+      y: [0,0,100,100].mult(size)
+      z: [0,0,0,0]
+    width: [0,4,4,0].mult(size)
+    height: [0,4,4,0].mult(size)
+    texture: [2,2,2]   
+    vertical: true 
