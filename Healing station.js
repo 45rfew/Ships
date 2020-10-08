be1f31ea496a5cb50d4b8f2568d717305f66fe3f
45rfew <48343592+45rfew@users.noreply.github.com>
2020-10-08 12:09:44 +1100

    Create Healing station.js

---
+++ Healing station.js
@@ -0,0 +1,100 @@
+return model =
+  bodies:
+    base:
+      section_segments: 14
+      offset:
+        x: 0
+        y: 0
+        z: 0
+      position:
+        x: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        y: [-40,-40,-35,-12,-8,-8,5,10,10,10,10,15,15,12,12]
+        z: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+      width: [0,45,45,37,33,33,33,33,33,33,33,27,25,25,0]
+      height: [0,45,45,37,33,33,33,33,33,33,33,27,25,25,0]
+      texture: [4,4,[10],3,3,3,3,4,1,63,63,17,63,4]
+      vertical: true
+    icon:
+      section_segments: [45,45+90,45+90*2,45+90*3]
+      offset:
+        x: 0
+        y: 7
+        z: 0
+      position:
+        x: [0,0,0,0,0]
+        y: [-10,-10,6,10,10]
+        z: [0,0,0,0,0]
+      width: [0,30,30,8,0]
+      height: [0,6,6,6,0]
+      texture: [17]
+      vertical: true
+    icondeco:
+      section_segments: [45,45+90,45+90*2,45+90*3]
+      offset:
+        x: 0
+        y: 7.1
+        z: 0
+      position:
+        x: [0,0,0,0,0]
+        y: [-10,-10,6,10,10]
+        z: [0,0,0,0,0]
+      width: [0,30,30,8,0]
+      height: [0,4,4,5,0]
+      texture: [63]
+      vertical: true      
+    icon2:
+      section_segments: [45,45+90,45+90*2,45+90*3]
+      offset:
+        x: 0
+        y: 7
+        z: 0
+      position:
+        x: [0,0,0,0,0]
+        y: [-10,-10,6,10,10]
+        z: [0,0,0,0,0]
+      width: [0,6,6,6,0]
+      height: [0,30,30,8,0]
+      texture: [17]
+      vertical: true
+    icon2deco:
+      section_segments: [45,45+90,45+90*2,45+90*3]
+      offset:
+        x: 0
+        y: 7.1
+        z: 0
+      position:
+        x: [0,0,0,0,0]
+        y: [-10,-10,6,10,10]
+        z: [0,0,0,0,0]
+      width: [0,4,4,5,0]
+      height: [0,30,30,8,0]
+      texture: [63]
+      vertical: true      
+    hub:
+      section_segments: 8
+      offset:
+        x: 30-5
+        y: -17
+        z: -25+5
+      position:
+        x: [0,0,0,0,0]
+        y: [-10,-10,10,11,9]
+        z: [0,0,0,0,0]
+      width: [0,7,7,6,0]
+      height: [0,7,7,6,0]
+      texture: [4,4,18,17]
+      vertical: true
+    hub2:
+      section_segments: 8
+      offset:
+        x: 0
+        y: -17
+        z: 32
+      position:
+        x: [0,0,0,0,0]
+        y: [-10,-10,10,11,9]
+        z: [0,0,0,0,0]
+      width: [0,7,7,6,0]
+      height: [0,7,7,6,0]
+      texture: [4,4,18,17]
+      vertical: true
